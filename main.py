6daa9a68b648500c8bc589d2c195a6a48900f01e
rui0920 <oceanbear0920@gmail.com>
2026-03-11 16:21:42 +0800

    feat: Implement basic LLM interaction using ChatOpenAI and print its response after loading environment variables.

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
+from langchain_ollama import ChatOllama
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+load_dotenv()
+
+
+
 def main():
-    print("Hello from rui!")
+    lim = ChatOpenAI(model="gpt-4o-mini",temperature=0.7)
+    reponse = lim.invoke("6")
+    print(reponse.content)
 
 
 if __name__ == "__main__":
